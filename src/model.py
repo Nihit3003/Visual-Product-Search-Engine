42c1dcc7a641c13895cc3085e6c78f35773bc250
Nihit <nihit300305@gmail.com>
2026-05-10 10:38:09 +0530

    Update model.py

--- src/model.py
+++ src/model.py
@@ -154,13 +154,13 @@ class SupConLoss(nn.Module):
 
 class VisualSearchModel(nn.Module):
 
-    CLIP_MODEL = "ViT-L-14"
+    CLIP_MODEL = "ViT-B-16"
 
     CLIP_PRETRAIN = "openai"
 
     def __init__(
         self,
-        clip_model_name="ViT-L-14",
+        clip_model_name="ViT-B-16",
         pretrained="openai",
         alpha=0.7,
         unfreeze_last_n=4,
